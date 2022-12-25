2569984524f28dd396ca16e5dce83f806ceb12a7
Filip Kraljevski <filipce001@gmail.com>
2022-12-25 10:08:43 +0100

    deleted comments on AdminController and refactored DataHolder to fill databese only once locally

--- Domashna 3/backend/src/main/java/mk/ukim/finki/backend/bootsrap/DataHolder.java
+++ Domashna 3/backend/src/main/java/mk/ukim/finki/backend/bootsrap/DataHolder.java
@@ -1,89 +1,41 @@
-/*package mk.ukim.finki.backend.bootsrap;
-
-import com.opencsv.CSVWriter;
+package mk.ukim.finki.backend.bootsrap;
 import jakarta.annotation.PostConstruct;
 import mk.ukim.finki.backend.model.Restaurant;
-import mk.ukim.finki.backend.model.User;
+import mk.ukim.finki.backend.repository.RestaurantRepository;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class DataHolder {
-    public static List<Restaurant> restaurants = new ArrayList<>();
-    public static List<User> users = new ArrayList<>();
+    private final RestaurantRepository restaurantRepository;
+
+    public DataHolder(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
 
     @PostConstruct
-    public void init(){
-        String path = "./database/RestourantsInSkopje.csv";
-        File file = new File(path);
-        try {
-            BufferedReader bf = new BufferedReader(new FileReader(file));
-            String line = bf.readLine();
-            while ((line = bf.readLine()) != null) {
-                String[] parts = line.split(",");
-                String name = parts[0];
-                String suburb = parts[1];
-                String street = parts[2];
-                Double lon = Double.parseDouble(parts[3]);
-                Double lat = Double.parseDouble(parts[4]);
-                String opens = parts[5];
-                String closes = parts[6];
-                String website = parts[7];
-                String phone = parts[8];
-                restaurants.add(new Restaurant(name, suburb, street, lon, lat, opens, closes, website, phone));
+    public void fillDatabaseOnlyOnceLocally() throws IOException {
+        if (restaurantRepository.count() == 0) {
+            File file = new File("database/RestourantsInSkopje.csv");
+            if (file.exists()) {
+                BufferedReader bf = new BufferedReader(new FileReader(file));
+                String line = bf.readLine();
+                while ((line = bf.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    String name = parts[0];
+                    String suburb = parts[1];
+                    String street = parts[2];
+                    Double lon = Double.parseDouble(parts[3]);
+                    Double lat = Double.parseDouble(parts[4]);
+                    String opens = parts[5];
+                    String closes = parts[6];
+                    String website = parts[7];
+                    String phone = parts[8];
+                    Restaurant r = new Restaurant(name, suburb, street, lat, lon, opens, closes, website, phone);
+                    restaurantRepository.save(r);
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        path = "./database/User.csv";
-        file = new File(path);
-        try{
-            BufferedReader bf = new BufferedReader(new FileReader(file));
-            String line = bf.readLine();
-            while ((line = bf.readLine()) != null) {
-                String[] parts = line.split(",");
-                String username = parts[0];
-                String email = parts[0];
-                String password = parts[0];
-                users.add(new User(username, email, password));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-
-    public static void saveRestaurant(Restaurant restaurant){
-        String path = "./database/RestourantsInSkopje.csv";
-        File file = new File(path);
-        try {
-            FileWriter fileWriter = new FileWriter(file);
-            CSVWriter writer = new CSVWriter(fileWriter);
-            String[] data = {restaurant.getName(), restaurant.getSuburb(), restaurant.getStreet(), restaurant.getLon().toString(),
-                    restaurant.getLat().toString(), restaurant.getOpens(), restaurant.getCloses(), restaurant.getWebsite(),
-                    restaurant.getPhone()};
-            writer.writeNext(data);
-            writer.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void saveUser(User user){
-        String path = "./database/User.csv";
-        File file = new File(path);
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(file);
-            CSVWriter writer = new CSVWriter(fileWriter);
-            String[] data = {user.getUsername(), user.getEmail(), user.getPassword()};
-            writer.writeNext(data);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}*/
+}
--- Domashna 3/backend/src/main/java/mk/ukim/finki/backend/web/AdminController.java
+++ Domashna 3/backend/src/main/java/mk/ukim/finki/backend/web/AdminController.java
@@ -1,17 +1,14 @@
 package mk.ukim.finki.backend.web;
 
 import mk.ukim.finki.backend.model.Exceptions.InvalidArgumentsException;
-import mk.ukim.finki.backend.model.Restaurant;
 import mk.ukim.finki.backend.service.RestaurantService;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.*;
 
 @Controller
 @RequestMapping("/admin")
@@ -41,35 +38,4 @@ public class AdminController {
             return "admin-panel";
         }
     }
-
-    //Ako e popolneta bazata, izbrisi go metodot
-    /*@PostMapping
-    public String fillDatabase(){
-
-        File file = new File("database/RestourantsInSkopje.csv");
-        if(file.exists()){
-            try {
-                BufferedReader bf = new BufferedReader(new FileReader(file));
-                String line = bf.readLine();
-                while ((line = bf.readLine()) != null) {
-                    System.out.println(line);
-
-                    String[] parts = line.split(",");
-                    String name = parts[0];
-                    String suburb = parts[1];
-                    String street = parts[2];
-                    Double lon = Double.parseDouble(parts[3]);
-                    Double lat = Double.parseDouble(parts[4]);
-                    String opens = parts[5];
-                    String closes = parts[6];
-                    String website = parts[7];
-                    String phone = parts[8];
-                    restaurantService.saveRestaurant(name, suburb, street, lat, lon, opens, closes, website, phone);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return "admin-panel";
-    }*/
 }
