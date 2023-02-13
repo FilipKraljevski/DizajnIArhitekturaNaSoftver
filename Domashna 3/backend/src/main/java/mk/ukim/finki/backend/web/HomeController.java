c4634e59e4d762adaafde3e5d7850c6693190db2
Martin <martinmileski4@gmail.com>
2023-02-13 21:43:02 +0100

    code refactoring

--- Domashna 3/backend/src/main/java/mk/ukim/finki/backend/web/HomeController.java
+++ Domashna 3/backend/src/main/java/mk/ukim/finki/backend/web/HomeController.java
@@ -10,13 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping(value = {"/","/home"})
+@RequestMapping(value = { "/", "/home" })
 public class HomeController {
 
     private final RestaurantService restaurantService;
@@ -25,48 +23,49 @@ public class HomeController {
         this.restaurantService = restaurantService;
     }
 
-
     @GetMapping
-    public String getHomePage(){
+    public String getHomePage() {
         return "home";
     }
 
     @PostMapping("/restaurants")
     public String getRestaurants(HttpServletRequest request, Model model) {
-        List<Restaurant>restaurants;
-        if(request.getParameter("ime").equals("") && request.getParameter("naselba").equals("")){
-            restaurants =  restaurantService.findAll();
-        }else if(request.getParameter("ime").equals("") && !request.getParameter("naselba").equals("")){
+        List<Restaurant> restaurants;
+        if (request.getParameter("ime").equals("") && request.getParameter("naselba").equals("")) {
+            restaurants = restaurantService.findAll();
+        } else if (request.getParameter("ime").equals("") && !request.getParameter("naselba").equals("")) {
             restaurants = restaurantService.findBySuburb(request.getParameter("naselba"));
-        }else if(!request.getParameter("ime").equals("") && request.getParameter("naselba").equals("")){
+        } else if (!request.getParameter("ime").equals("") && request.getParameter("naselba").equals("")) {
             restaurants = restaurantService.findByName(request.getParameter("ime"));
-        }else{
-            restaurants = restaurantService.findAllByNameAndSuburb(request.getParameter("ime"),request.getParameter("naselba"));
+        } else {
+            restaurants = restaurantService.findAllByNameAndSuburb(request.getParameter("ime"),
+                    request.getParameter("naselba"));
         }
-        model.addAttribute("restaurants",restaurants);
+        model.addAttribute("restaurants", restaurants);
         return "home";
     }
 
     @GetMapping("/nearest")
-    public String getNearest(HttpServletRequest request,Model model){
+    public String getNearest(HttpServletRequest request, Model model) {
         List<Restaurant> restaurants = restaurantService.findAll();
         double lon = Double.parseDouble(request.getParameter("lon"));
         double lat = Double.parseDouble(request.getParameter("lat"));
         List<Double> distances = new ArrayList<>();
-        for (int i=0;i<restaurants.size();i++){
-            distances.add(Math.sqrt(Math.pow(lon-restaurants.get(i).getLon(),2)+ Math.pow(lat-restaurants.get(i).getLat(),2)));
+        for (int i = 0; i < restaurants.size(); i++) {
+            distances.add(Math.sqrt(
+                    Math.pow(lon - restaurants.get(i).getLon(), 2) + Math.pow(lat - restaurants.get(i).getLat(), 2)));
         }
-        List<Restaurant>nearestRestourants = new ArrayList<>();
-        List<Double>sortedDistances = distances.stream().sorted().collect(Collectors.toList());
+        List<Restaurant> nearestRestourants = new ArrayList<>();
+        List<Double> sortedDistances = distances.stream().sorted().collect(Collectors.toList());
         sortedDistances = sortedDistances.stream().limit(5).collect(Collectors.toList());
-        for(int i=0;i<5;i++){
-            for(int j=0;j<distances.size();j++){
-                if(sortedDistances.get(i) == distances.get(j)){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < distances.size(); j++) {
+                if (sortedDistances.get(i) == distances.get(j)) {
                     nearestRestourants.add(restaurants.get(j));
                 }
             }
         }
-        model.addAttribute("restaurants",nearestRestourants);
+        model.addAttribute("restaurants", nearestRestourants);
         System.out.println(distances);
         return "home";
     }
